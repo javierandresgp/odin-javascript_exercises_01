ad0057443ef8ad9deb33ddfa8eac3c5ebc44740b
javierandresgp <javierandresgarzonpatarroyo@gmail.com>
2022-10-18 21:26:36 -0500

    my solution

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function (arr, ...items) {
-  return;
+const removeFromArray = function (...args) {
+  const arr = args[0];
+  for (const [i, arg] of args.entries()) {
+    if (i > 0) {
+      const exists = arr.includes(arg);
+      if (exists) {
+        const removed = arr.splice(i, 1);
+        console.log('removed:', removed);
+      }
+    }
+  }
+  console.log('arr:', arr);
+  return arr;
 };
 
 // Do not edit below this line
